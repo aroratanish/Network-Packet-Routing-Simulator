1fe1843e6818111b9b3d4151a4837e30f233eae5
Tanish Arora <tarora_be24@thapar.edu>
2026-03-28 21:23:10 +0530

    Create graph.py

---
+++ graph.py
@@ -0,0 +1,16 @@
+class Graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_edge(self, u, v, weight):
+        if u not in self.graph:
+            self.graph[u] = []
+        if v not in self.graph:
+            self.graph[v] = []
+
+        self.graph[u].append((v, weight))
+        self.graph[v].append((u, weight))  # undirected
+
+    def display(self):
+        for node in self.graph:
+            print(node, "->", self.graph[node])
